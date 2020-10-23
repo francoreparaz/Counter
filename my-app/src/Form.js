f270f4bcdffc8cf382bc48567f1ed4309902bb18
Franco <you@example.com>
2020-10-23 01:36:49 -0300

    changes

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,19 +1,32 @@
 import React from "react";
 import {useState} from "react";
 import "./Form.css"
+import style from "react"
 
 export default function FormCounter(){
     const [state,setState]=useState(0)
+    if(state<0){
+       var elem = document.getElementById("1")
+       elem.style.color="red"
+    }
+    if(state>0){
+        var elem = document.getElementById("1")
+           elem.style.color="green"
+    }
+        
+    
     return(
         <div className="Form">
-            <form onSubmit={e=>e.preventDefault()}>
+            <form key="form" onSubmit={e=>e.preventDefault()}>
                 <input 
+                key="input"
+                id="1"
                 className="counter"
                 type= "number"
                 value={state}
                 />
-                <button className="botonMas" onClick={e=>{setState(state + 1)}}> + increment</button>
-                <button className="botonMenos" onClick={e=>{setState(state - 1)}}> - decrement</button>
+                <button key="button" className="botonMas" onClick={e=>{setState(state + 1)}}> + increment</button>
+                <button  key="button2"  className="botonMenos" onClick={e=>{setState(state - 1)}}> - decrement</button>
             </form>
         </div>
     )
